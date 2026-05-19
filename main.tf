736d221d94a69ad457d597850e3fcf7b288b6054
Andrew <andrewthcook@gmail.com>
2026-05-19 19:28:52 -0400

    new module

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+module "my_application" {
+  source = "./modules/my-app"
+
+  # Pass the bucket name into the module variable
+  app_bucket_name = aws_s3_bucket.my_storage_bucket.id
+}
+
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
